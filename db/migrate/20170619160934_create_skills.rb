6305cf2d4effd70560615fdc6d6adc329fceef6c
Jordan Hudgens <jordan@devcamp.com>
2017-06-20 15:34:22 +0000

    Added portfolio items via resource generator

--- db/migrate/20170619160934_create_skills.rb
+++ db/migrate/20170619160934_create_skills.rb
@@ -1,6 +1,6 @@
 class CreateSkills < ActiveRecord::Migration[5.0]
   def change
-    create_table :Skills do |t|
+    create_table :skills do |t|
       t.string :title
       t.integer :percent_utilized
 
