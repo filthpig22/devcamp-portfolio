e3860032736935ea929e50b476b9eaee51ca525a
Jordan Hudgens <jordan@devcamp.com>
2017-06-26 17:07:36 +0000

    Integrated friendly routes for blog posts

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,14 +1,15 @@
+
 class PortfoliosController < ApplicationController
   def index
     @portfolio_items = Portfolio.all
   end
-  
+
   def new
     @portfolio_item = Portfolio.new
   end
-  
+
   def create
-     @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
+    @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
 
     respond_to do |format|
       if @portfolio_item.save
@@ -18,14 +19,14 @@ class PortfoliosController < ApplicationController
       end
     end
   end
-  
+
   def edit
     @portfolio_item = Portfolio.find(params[:id])
   end
-  
-    def update
+
+  def update
     @portfolio_item = Portfolio.find(params[:id])
-    
+
     respond_to do |format|
       if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
         format.html { redirect_to portfolios_path, notice: 'The record successfully updated.' }
@@ -33,19 +34,19 @@ class PortfoliosController < ApplicationController
         format.html { render :edit }
       end
     end
-    end
-  
+  end
+
   def show
     @portfolio_item = Portfolio.find(params[:id])
   end
-  
+
   def destroy
     # Perform the lookup
     @portfolio_item = Portfolio.find(params[:id])
-    
-    # Destroy/Delete the record
+
+    # Destroy/delete the record
     @portfolio_item.destroy
-    
+
     # Redirect
     respond_to do |format|
       format.html { redirect_to portfolios_url, notice: 'Record was removed.' }
