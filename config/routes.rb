51d296bd9b7deaa32a0aab0bb96414044d7ab22a
Jordan Hudgens <jordan@devcamp.com>
2017-07-10 16:27:15 +0000

    changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
-  resources :portfolios
-  get 'pages/home'
-
-  get 'pages/about'
-
-  get 'pages/contact'
-
-  resources :blogs
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+   resources :portfolios, except: [:show]
+  get 'angular-items', to: 'portfolios#angular'
+   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
+   
+   resources :blogs, except: [:show]
+  get 'angular-items', to: 'blogs#angular'
+   get 'blogs/:id', to: 'blogs#show', as: 'blogs_show'
+   
+   get 'about-me', to: 'pages#about'
+   get 'contact', to: 'pages#contact'
+   
+   root to: 'pages#home'
+   
 end
